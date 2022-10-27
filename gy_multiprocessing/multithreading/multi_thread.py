34cdda5118666b30a360975cd470003d7f4d01a2
Guangyu He <fredhgy2010lgmail.com>
2022-10-27 20:43:27 +0200

    update: descriptions

--- gy_multiprocessing/multithreading/multi_thread.py
+++ gy_multiprocessing/multithreading/multi_thread.py
@@ -7,15 +7,30 @@ class MultiThread:
         self.max_threads = cpu_count() - 1
 
     def init(self):
+        """
+        :return: multi threads object and a threads list
+        """
         mt = Pool(processes=self.max_threads)
         pool_list: list = []
         return mt, pool_list
 
     def add(self, mt, pool_list: list, func, args: tuple):
+        """
+        :param mt:  multi threads object
+        :param pool_list: threads list
+        :param func: the function to be called for multiprocessing
+        :param args: the arguments of the function
+        :return: a list with added tasks
+        """
         pool_list.append(mt.apply_async(func, args))
         return pool_list
 
     def run(self, mt, pool_list: list):
+        """
+        :param mt:  multi threads object
+        :param pool_list: threads list
+        :return: the result list from each tasks
+        """
         start = time.time()
         mt.close()
         mt.join()
