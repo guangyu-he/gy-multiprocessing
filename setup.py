35d4ae9df7256bd55018d3daaf426c13c1b9f329
Guangyu He <guangyu.he@golden-tech.de>
2023-04-12 11:22:58 +0200

    fix: long desc issue

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
+from pathlib import Path
 
-with open('README.md') as fh:
-    long_description = fh.read()
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="gy_multiprocessing",
